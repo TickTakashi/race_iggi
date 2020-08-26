5c3f8f6ec4f303708c46d7d8997f04fdae5c25e5
chronospherics <69740169+chronospherics@users.noreply.github.com>
2020-08-26 17:45:54 +0100

    Stuff

--- Assets/Scripts/BlockRotator.cs
+++ Assets/Scripts/BlockRotator.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BlockRotator : MonoBehaviour
-{
+ {
 
-    public float speed = 150f;
+    // public float speed = 150f;
+    Animation anim;
+    public float animSpeed = 0.001f;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody>().angularVelocity = Vector3.up * speed;
+        //  GetComponent<Rigidbody>().angularVelocity = Vector3.up * speed;
+        anim = GetComponent<Animation>();
+        anim["spin2"].speed = animSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
        // transform.Rotate(Vector3.up, speed * Time.deltaTime);
-    }
-}
+    
+}} 
+
+
+
