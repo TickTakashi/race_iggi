72189041ebe0ee219e1073ad7bfe62d2a75703b4
Charles Gbadamosi <takashigbadamosi@gmail.com>
2020-09-03 15:45:49 +0100

    Adjusts spinner to use physics.

---
+++ Assets/Scripts/SpinnerGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class SpinnerGenerator : NetworkBehaviour
+{
+	public Rigidbody[] Spinners;
+
+	public float minForce = 5f;
+	public float maxForce = 15f;
+
+	public override void OnStartServer() {
+		base.OnStartServer();
+		Setup();
+	}
+
+	void Setup() {
+		foreach (Rigidbody rb in Spinners) {
+			rb.angularVelocity = rb.transform.right * Random.Range(minForce, maxForce);
+		}
+	}
+}
